aff2638883cce1bac5852c2ed8edb1673e82b281
Jonathan Duong <djonathan91.77@gmail.com>
2023-05-30 15:51:45 +0200

    Add documentations in viewmodels

--- DEMO_CoreDataMultipleEntities/ViewModel/CharacterViewModel.swift
+++ DEMO_CoreDataMultipleEntities/ViewModel/CharacterViewModel.swift
@@ -8,19 +8,36 @@
 import Foundation
 import CoreData
 
+/// `CharacterViewModel` is an ObservableObject that manages the creation, deletion, and retrieval of `Character` objects.
 class CharacterViewModel: ObservableObject {
+
+    /// An array of `Character` that contains the list of characters fetched from the database.
     @Published var characters: [Character] = []
+
+    /// A string that stores the name of the new character to be added to the database.
     @Published var name: String = ""
+
+    /// A string that stores the description of the new character to be added to the database.
     @Published var description: String = ""
+
+    /// The `NSManagedObjectContext` used for interacting with the Core Data database.
     var viewContext: NSManagedObjectContext
+
+    /// The `Manga` that the characters belong to.
     let manga: Manga
 
+    /// Initializes a new `CharacterViewModel` object.
+    ///
+    /// - Parameters:
+    ///   - viewContext: The `NSManagedObjectContext` to use for interacting with the database.
+    ///   - manga: The `Manga` that the characters belong to.
     init(viewContext: NSManagedObjectContext, manga: Manga) {
         self.viewContext = viewContext
         self.manga = manga
         fetchCharacters()
     }
 
+    /// Fetches characters from the database and sorts them by name in ascending order.
     private func fetchCharacters() {
         let fetchRequest: NSFetchRequest<Character> = Character.fetchRequest()
         fetchRequest.sortDescriptors = [NSSortDescriptor(keyPath: \Character.name, ascending: true)]
@@ -31,6 +48,8 @@ class CharacterViewModel: ObservableObject {
         }
     }
 
+    /// Creates a new `Character` object with the name and description stored in `name` and `description`, adds it to the database,
+    /// then refreshes the list of characters.
     func addCharacter() {
         let newCharacter = Character(context: viewContext)
         newCharacter.name = name
@@ -40,6 +59,10 @@ class CharacterViewModel: ObservableObject {
         fetchCharacters()
     }
 
+    /// Deletes characters at the specified indices from the database,
+    /// then refreshes the list of characters.
+    ///
+    /// - Parameter offsets: The indices of the characters to delete.
     func deleteCharacters(at offsets: IndexSet) {
         offsets.forEach { index in
             let character = characters[index]
@@ -49,6 +72,8 @@ class CharacterViewModel: ObservableObject {
         fetchCharacters()
     }
 
+    /// Saves the current context.
+    /// If the context cannot be saved, an error is printed.
     private func saveContext() {
         do {
             try viewContext.save()
@@ -57,5 +82,3 @@ class CharacterViewModel: ObservableObject {
         }
     }
 }
-
-
--- DEMO_CoreDataMultipleEntities/ViewModel/MangaViewModel.swift
+++ DEMO_CoreDataMultipleEntities/ViewModel/MangaViewModel.swift
@@ -8,16 +8,24 @@
 import Foundation
 import CoreData
 
+/// `MangaViewModel` is an ObservableObject that manages the creation, deletion, and retrieval of `Manga` objects.
 class MangaViewModel: ObservableObject {
+    /// An array of `Manga` that contains the list of mangas fetched from the database.
     @Published var mangas: [Manga] = []
+    /// A string that stores the name of the new manga to be added to the database.
     @Published var newManga: String = ""
+    /// The `NSManagedObjectContext` used for interacting with the Core Data database.
     private var viewContext: NSManagedObjectContext
 
+    /// Initializes a new `MangaViewModel` object.
+    ///
+    /// - Parameter viewContext: The `NSManagedObjectContext` to use for interacting with the database.
     init(viewContext: NSManagedObjectContext) {
         self.viewContext = viewContext
         fetchMangas()
     }
 
+    /// Fetches mangas from the database and sorts them by name in ascending order.
     private func fetchMangas() {
         let fetchRequest: NSFetchRequest<Manga> = Manga.fetchRequest()
         fetchRequest.sortDescriptors = [NSSortDescriptor(keyPath: \Manga.name, ascending: true)]
@@ -28,6 +36,8 @@ class MangaViewModel: ObservableObject {
         }
     }
 
+    /// Creates a new `Manga` object with the name stored in `newManga`, adds it to the database,
+    /// then refreshes the list of mangas and resets `newManga` to an empty string.
     func addManga() {
         let manga = Manga(context: viewContext)
         manga.name = newManga
@@ -37,6 +47,10 @@ class MangaViewModel: ObservableObject {
         newManga = ""
     }
 
+    /// Deletes mangas at the specified indices from the database,
+    /// then refreshes the list of mangas.
+    ///
+    /// - Parameter offsets: The indices of the mangas to delete.
     func deleteMangas(offsets: IndexSet) {
         offsets.forEach { index in
             let manga = mangas[index]
@@ -46,6 +60,8 @@ class MangaViewModel: ObservableObject {
         fetchMangas()
     }
 
+    /// Saves the current context.
+    /// If the context cannot be saved, an error is printed.
     private func saveContext() {
         do {
             try viewContext.save()
